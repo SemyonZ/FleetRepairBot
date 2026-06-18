3c04467073f711b134e14d51e449354194aed1bd
SemyonZ <Errormini@gmail.com>
2026-06-18 13:43:50 +0300

    Add src/FleetRepairBot.Domain/Entities/Vehicle.cs

---
+++ src/FleetRepairBot.Domain/Entities/Vehicle.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FleetRepairBot.Domain.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string PlateNumber { get; set; }
+        public string Model { get; set; }
+    }
+}
