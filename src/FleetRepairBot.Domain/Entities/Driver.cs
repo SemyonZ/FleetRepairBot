72e9427a7c8285e1672ea21d559cb570dee8e96c
SemyonZ <Errormini@gmail.com>
2026-06-18 13:43:48 +0300

    Add src/FleetRepairBot.Domain/Entities/Driver.cs

---
+++ src/FleetRepairBot.Domain/Entities/Driver.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FleetRepairBot.Domain.Entities
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
