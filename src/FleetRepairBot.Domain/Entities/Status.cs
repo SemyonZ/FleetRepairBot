5b6010dc8d5aa227f86fd1e8652461b75db7c5b9
SemyonZ <Errormini@gmail.com>
2026-06-18 13:43:51 +0300

    Add src/FleetRepairBot.Domain/Entities/Status.cs

---
+++ src/FleetRepairBot.Domain/Entities/Status.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FleetRepairBot.Domain.Entities
+{
+    public enum Status
+    {
+        New = 0,
+        InProgress = 1,
+        Completed = 2,
+        Cancelled = 3
+    }
+}
