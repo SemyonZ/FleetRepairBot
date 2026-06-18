c978bcfef8ab299d00d464ddf4b78a6742638dc2
SemyonZ <Errormini@gmail.com>
2026-06-18 14:16:23 +0300

    Update Driver.cs

--- src/FleetRepairBot.Domain/Entities/Driver.cs
+++ src/FleetRepairBot.Domain/Entities/Driver.cs
@@ -5,7 +5,7 @@ namespace FleetRepairBot.Domain.Entities
     public class Driver
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
+         public string Name { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
     }
 }
