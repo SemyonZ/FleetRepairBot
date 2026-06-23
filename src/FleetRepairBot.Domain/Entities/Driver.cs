4285db30d43a5d7e787c1d4c19e281da69e316a1
SemyonZ <Errormini@gmail.com>
2026-06-23 11:31:53 +0300

    Fix build error in src/FleetRepairBot.Domain/Entities/Driver.cs

--- src/FleetRepairBot.Domain/Entities/Driver.cs
+++ src/FleetRepairBot.Domain/Entities/Driver.cs
@@ -5,9 +5,7 @@ namespace FleetRepairBot.Domain.Entities
     public class Driver
     {
         public int Id { get; set; }
-         public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
-        THIS_IS_BUILD_TEST_ERROR
     }
 }
-
