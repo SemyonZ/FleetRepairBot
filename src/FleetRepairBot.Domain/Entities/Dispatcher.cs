9da42ca09a0623a6a971b97bf2a3e80cab730e5f
SemyonZ <Errormini@gmail.com>
2026-06-24 13:29:59 +0300

    Update src/FleetRepairBot.Domain/Entities/Dispatcher.cs

--- src/FleetRepairBot.Domain/Entities/Dispatcher.cs
+++ src/FleetRepairBot.Domain/Entities/Dispatcher.cs
@@ -4,17 +4,10 @@ namespace FleetRepairBot.Domain.Entities
 {
     public class Dispatcher
     {
-        public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public string Contact { get; private set; }
-
-        protected Dispatcher() { }
-
-        public Dispatcher(Guid id, string name, string contact)
-        {
-            Id = id == default ? Guid.NewGuid() : id;
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-            Contact = contact ?? string.Empty;
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Required: Telegram identifier for the dispatcher
+        public long TelegramId { get; set; }
+        // Note: intentionally no Email property as per requirements
     }
 }
