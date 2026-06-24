82b48bea7e728a7f63511a070d88c0add59a0ba5
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:24 +0300

    Update src/FleetRepairBot.Data/Repositories/IDriverRepository.cs

--- src/FleetRepairBot.Data/Repositories/IDriverRepository.cs
+++ src/FleetRepairBot.Data/Repositories/IDriverRepository.cs
@@ -1,12 +1,14 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using FleetRepairBot.Domain.Entities;
+using System;
+using System.Threading.Tasks;
 
 namespace FleetRepairBot.Data.Repositories
 {
     public interface IDriverRepository
     {
-        Task<Driver> GetByIdAsync(int id);
-        Task<IEnumerable<Driver>> GetAllAsync();
+        Task<Driver> GetByIdAsync(Guid id);
+        Task AddAsync(Driver driver);
+        Task UpdateAsync(Driver driver);
+        Task SaveChangesAsync();
     }
 }
