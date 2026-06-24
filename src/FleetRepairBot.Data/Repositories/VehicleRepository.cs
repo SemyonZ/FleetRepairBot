388b39e37a902cafdee6afe13d4f3427db9fcdb4
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:28 +0300

    Update src/FleetRepairBot.Data/Repositories/VehicleRepository.cs

--- src/FleetRepairBot.Data/Repositories/VehicleRepository.cs
+++ src/FleetRepairBot.Data/Repositories/VehicleRepository.cs
@@ -1,23 +1,22 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using FleetRepairBot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace FleetRepairBot.Data.Repositories
 {
     public class VehicleRepository : IVehicleRepository
     {
         private readonly FleetRepairDbContext _db;
-        public VehicleRepository(FleetRepairDbContext db) { _db = db; }
+        public VehicleRepository(FleetRepairDbContext db) => _db = db;
 
-        public async Task<IEnumerable<Vehicle>> GetAllAsync()
+        public async Task AddAsync(Vehicle vehicle) => await _db.Vehicles.AddAsync(vehicle);
+        public async Task<Vehicle> GetByIdAsync(Guid id) => await _db.Vehicles.FirstOrDefaultAsync(v => v.Id == id);
+        public Task UpdateAsync(Vehicle vehicle)
         {
-            return await _db.Vehicles.ToListAsync();
+            _db.Vehicles.Update(vehicle);
+            return Task.CompletedTask;
         }
-
-        public async Task<Vehicle> GetByIdAsync(int id)
-        {
-            return await _db.Vehicles.FindAsync(id);
-        }
+        public async Task SaveChangesAsync() => await _db.SaveChangesAsync();
     }
 }
