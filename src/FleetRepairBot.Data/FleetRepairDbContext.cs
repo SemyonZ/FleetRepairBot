ae90eeb28073ce9a7410d990d3f2f7a06c80e5e9
SemyonZ <Errormini@gmail.com>
2026-06-24 13:30:08 +0300

    Update src/FleetRepairBot.Data/FleetRepairDbContext.cs

--- src/FleetRepairBot.Data/FleetRepairDbContext.cs
+++ src/FleetRepairBot.Data/FleetRepairDbContext.cs
@@ -1,5 +1,5 @@
-using FleetRepairBot.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using FleetRepairBot.Domain.Entities;
 
 namespace FleetRepairBot.Data
 {
@@ -7,40 +7,25 @@ namespace FleetRepairBot.Data
     {
         public FleetRepairDbContext(DbContextOptions<FleetRepairDbContext> options) : base(options) { }
 
+        // DbSets for all entities used by repositories and the application
         public DbSet<RepairRequest> RepairRequests { get; set; }
         public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Dispatcher> Dispatchers { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Status> Statuses { get; set; }
         public DbSet<RequestPhoto> RequestPhotos { get; set; }
         public DbSet<StatusHistory> StatusHistories { get; set; }
         public DbSet<AuditLog> AuditLogs { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<RepairRequest>(b =>
-            {
-                b.HasKey(x => x.Id);
-                b.Property(x => x.Description).HasMaxLength(2000);
-                b.OwnsMany<RequestPhoto>(
-                    "Photos",
-                    pb =>
-                    {
-                        pb.WithOwner().HasForeignKey("RepairRequestId");
-                        pb.Property<Guid>("Id");
-                    });
-            });
-
-            modelBuilder.Entity<RequestPhoto>(b =>
-            {
-                b.HasKey(x => x.Id);
-            });
-
-            modelBuilder.Entity<StatusHistory>(b => b.HasKey(x => x.Id));
-            modelBuilder.Entity<Driver>(b => b.HasKey(x => x.Id));
-            modelBuilder.Entity<Vehicle>(b => b.HasKey(x => x.Id));
-            modelBuilder.Entity<AuditLog>(b => b.HasKey(x => x.Id));
-
-            // store enum as int by default
             base.OnModelCreating(modelBuilder);
+
+            // Убираем провайдер-специфичную настройку для PerformedAt, чтобы не полагаться на GETUTCDATE()
+            // Предполагается, что AuditLog.PerformedAt инициализируется в C# (например, property = DateTime.UtcNow) или задаётся при создании сущности.
+
+            // Здесь можно добавить дополнительные конфигурации сущностей (индексы, ограничения и т.д.),
+            // если это понадобится в будущем.
         }
     }
 }
