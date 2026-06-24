158457dd697bf4b2456ea1e571b0d9f0f4f070a6
SemyonZ <Errormini@gmail.com>
2026-06-24 13:29:58 +0300

    Update src/FleetRepairBot.Domain/Entities/Driver.cs

--- src/FleetRepairBot.Domain/Entities/Driver.cs
+++ src/FleetRepairBot.Domain/Entities/Driver.cs
@@ -4,25 +4,9 @@ namespace FleetRepairBot.Domain.Entities
 {
     public class Driver
     {
-        public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public string Phone { get; private set; }
-        public string Email { get; private set; }
-
-        protected Driver() { }
-
-        public Driver(Guid id, string name, string phone, string email)
-        {
-            Id = id == default ? Guid.NewGuid() : id;
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-            Phone = phone ?? string.Empty;
-            Email = email ?? string.Empty;
-        }
-
-        public void UpdateContact(string phone, string email)
-        {
-            Phone = phone ?? Phone;
-            Email = email ?? Email;
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Required: Telegram identifier for the driver
+        public long TelegramId { get; set; }
     }
 }
