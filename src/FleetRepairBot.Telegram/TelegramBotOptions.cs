3e3a9cc71ba80dcbf893fd8f15b65686ff60d6b6
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:34 +0300

    Update src/FleetRepairBot.Telegram/TelegramBotOptions.cs

--- src/FleetRepairBot.Telegram/TelegramBotOptions.cs
+++ src/FleetRepairBot.Telegram/TelegramBotOptions.cs
@@ -1,16 +1,9 @@
-using System;
-
 namespace FleetRepairBot.Telegram
 {
     public class TelegramBotOptions
     {
-        // Token for the Telegram bot. If empty or whitespace, the hosted service will not start the bot.
-        public string BotToken { get; set; }
-
-        // Optional admin chat id for notifications (can be null)
-        public long? AdminChatId { get; set; }
-
-        // Directory where telegram-related files (e.g. uploaded photos) can be stored
-        public string StorageDirectory { get; set; } = "./BotStorage";
+        public string Token { get; set; }
+        public long[] AllowedChatIds { get; set; }
+        public bool UsePolling { get; set; } = true;
     }
 }
