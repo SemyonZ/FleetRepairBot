70c7729d32bab57043ab38b2525b85b8d23a35c7
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:25 +0300

    Update src/FleetRepairBot.Data/Repositories/DriverRepository.cs

--- src/FleetRepairBot.Data/Repositories/DriverRepository.cs
+++ src/FleetRepairBot.Data/Repositories/DriverRepository.cs
@@ -1,23 +1,22 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using FleetRepairBot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace FleetRepairBot.Data.Repositories
 {
     public class DriverRepository : IDriverRepository
     {
         private readonly FleetRepairDbContext _db;
-        public DriverRepository(FleetRepairDbContext db) { _db = db; }
+        public DriverRepository(FleetRepairDbContext db) => _db = db;
 
-        public async Task<IEnumerable<Driver>> GetAllAsync()
+        public async Task AddAsync(Driver driver) => await _db.Drivers.AddAsync(driver);
+        public async Task<Driver> GetByIdAsync(Guid id) => await _db.Drivers.FirstOrDefaultAsync(d => d.Id == id);
+        public Task UpdateAsync(Driver driver)
         {
-            return await _db.Drivers.ToListAsync();
+            _db.Drivers.Update(driver);
+            return Task.CompletedTask;
         }
-
-        public async Task<Driver> GetByIdAsync(int id)
-        {
-            return await _db.Drivers.FindAsync(id);
-        }
+        public async Task SaveChangesAsync() => await _db.SaveChangesAsync();
     }
 }
