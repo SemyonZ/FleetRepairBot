7af43edf2058aa52cff128067b193711a9d096a9
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:23 +0300

    Update src/FleetRepairBot.Data/Repositories/RepairRequestRepository.cs

--- src/FleetRepairBot.Data/Repositories/RepairRequestRepository.cs
+++ src/FleetRepairBot.Data/Repositories/RepairRequestRepository.cs
@@ -1,34 +1,44 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using FleetRepairBot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FleetRepairBot.Data.Repositories
 {
     public class RepairRequestRepository : IRepairRequestRepository
     {
         private readonly FleetRepairDbContext _db;
-        public RepairRequestRepository(FleetRepairDbContext db) { _db = db; }
+        public RepairRequestRepository(FleetRepairDbContext db) => _db = db;
 
         public async Task AddAsync(RepairRequest request)
         {
-            _db.RepairRequests.Add(request);
-            await _db.SaveChangesAsync();
+            await _db.RepairRequests.AddAsync(request);
         }
 
-        public async Task<IEnumerable<RepairRequest>> GetAllAsync()
+        public async Task<RepairRequest> GetByIdAsync(Guid id)
         {
-            return await _db.RepairRequests.Include(r => r.Photos).ToListAsync();
+            return await _db.RepairRequests
+                .Include(r => r.Photos)
+                .Include(r => r.StatusHistory)
+                .FirstOrDefaultAsync(r => r.Id == id);
         }
 
-        public async Task<RepairRequest> GetByIdAsync(int id)
+        public Task<IEnumerable<RepairRequest>> ListByStatusAsync(Status status)
         {
-            return await _db.RepairRequests.Include(r => r.Photos).FirstOrDefaultAsync(r => r.Id == id);
+            var q = _db.RepairRequests.Where(r => r.Status == status).AsQueryable();
+            return Task.FromResult<IEnumerable<RepairRequest>>(q.AsEnumerable());
         }
 
-        public async Task UpdateAsync(RepairRequest request)
+        public Task UpdateAsync(RepairRequest request)
         {
             _db.RepairRequests.Update(request);
+            return Task.CompletedTask;
+        }
+
+        public async Task SaveChangesAsync()
+        {
             await _db.SaveChangesAsync();
         }
     }
