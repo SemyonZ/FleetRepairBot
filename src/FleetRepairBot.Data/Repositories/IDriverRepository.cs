7a2b2602b5955c31ee2b3e1385726babf3df7da2
SemyonZ <Errormini@gmail.com>
2026-06-24 13:30:12 +0300

    Update src/FleetRepairBot.Data/Repositories/IDriverRepository.cs

--- src/FleetRepairBot.Data/Repositories/IDriverRepository.cs
+++ src/FleetRepairBot.Data/Repositories/IDriverRepository.cs
@@ -1,14 +1,16 @@
-using FleetRepairBot.Domain.Entities;
-using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using FleetRepairBot.Domain.Entities;
 
 namespace FleetRepairBot.Data.Repositories
 {
     public interface IDriverRepository
     {
-        Task<Driver> GetByIdAsync(Guid id);
-        Task AddAsync(Driver driver);
-        Task UpdateAsync(Driver driver);
-        Task SaveChangesAsync();
+        Task<Driver> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<Driver> GetByTelegramIdAsync(long telegramId, CancellationToken ct = default);
+        Task<IEnumerable<Driver>> GetAllAsync(CancellationToken ct = default);
+        Task AddAsync(Driver driver, CancellationToken ct = default);
+        Task UpdateAsync(Driver driver, CancellationToken ct = default);
     }
 }
