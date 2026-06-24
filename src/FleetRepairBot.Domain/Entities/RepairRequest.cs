7ce3b0a6708b89a01a95e0ef0031be922073d727
SemyonZ <Errormini@gmail.com>
2026-06-24 09:35:47 +0300

    Update src/FleetRepairBot.Domain/Entities/RepairRequest.cs

--- src/FleetRepairBot.Domain/Entities/RepairRequest.cs
+++ src/FleetRepairBot.Domain/Entities/RepairRequest.cs
@@ -5,12 +5,24 @@ namespace FleetRepairBot.Domain.Entities
 {
     public class RepairRequest
     {
-        public int Id { get; set; }
-        public int VehicleId { get; set; }
-        public int DriverId { get; set; }
-        public Status Status { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
-        public List<RequestPhoto> Photos { get; set; } = new List<RequestPhoto>();
+        public DateTime? UpdatedAt { get; set; }
+        public Status Status { get; set; }
+
+        public Guid? VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+
+        public Guid? DriverId { get; set; }
+        public Driver Driver { get; set; }
+
+        // who submitted the request (dispatcher)
+        public Guid? CreatedByDispatcherId { get; set; }
+        public Dispatcher CreatedByDispatcher { get; set; }
+
+        public ICollection<RequestPhoto> Photos { get; set; } = new List<RequestPhoto>();
+        public ICollection<StatusHistory> StatusHistories { get; set; } = new List<StatusHistory>();
     }
 }
