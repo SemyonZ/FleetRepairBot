74d008e58e402a0c4e845c16e1a5776db8e46b0d
SemyonZ <Errormini@gmail.com>
2026-06-24 13:30:00 +0300

    Update src/FleetRepairBot.Domain/Entities/Vehicle.cs

--- src/FleetRepairBot.Domain/Entities/Vehicle.cs
+++ src/FleetRepairBot.Domain/Entities/Vehicle.cs
@@ -4,22 +4,9 @@ namespace FleetRepairBot.Domain.Entities
 {
     public class Vehicle
     {
-        public Guid Id { get; private set; }
-        public string PlateNumber { get; private set; }
-        public string Model { get; private set; }
-
-        protected Vehicle() { }
-
-        public Vehicle(Guid id, string plateNumber, string model)
-        {
-            Id = id == default ? Guid.NewGuid() : id;
-            PlateNumber = plateNumber ?? throw new ArgumentNullException(nameof(plateNumber));
-            Model = model ?? string.Empty;
-        }
-
-        public void UpdateModel(string model)
-        {
-            Model = model ?? Model;
-        }
+        public int Id { get; set; }
+        public string VIN { get; set; }
+        public string LicensePlate { get; set; }
+        public string Model { get; set; }
     }
 }
