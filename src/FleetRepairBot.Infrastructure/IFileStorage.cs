acb8eba178fbc959d5e748c7dd3e4253e8bf3627
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:32 +0300

    Update src/FleetRepairBot.Infrastructure/IFileStorage.cs

--- src/FleetRepairBot.Infrastructure/IFileStorage.cs
+++ src/FleetRepairBot.Infrastructure/IFileStorage.cs
@@ -1,1 +1,13 @@
-/* content omitted in packaging */
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FleetRepairBot.Infrastructure
+{
+    public interface IFileStorage
+    {
+        Task<string> SaveAsync(Stream content, string relativePath);
+        Task DeleteAsync(string relativePath);
+        Task<Stream> OpenReadAsync(string relativePath);
+        string GetUri(string relativePath);
+    }
+}
