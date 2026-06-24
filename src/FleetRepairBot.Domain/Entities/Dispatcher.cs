14b7047af535a5c66c45b82afd72e8744271c71b
SemyonZ <Errormini@gmail.com>
2026-06-24 09:35:43 +0300

    Update src/FleetRepairBot.Domain/Entities/Dispatcher.cs

--- src/FleetRepairBot.Domain/Entities/Dispatcher.cs
+++ src/FleetRepairBot.Domain/Entities/Dispatcher.cs
@@ -4,8 +4,11 @@ namespace FleetRepairBot.Domain.Entities
 {
     public class Dispatcher
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Contact { get; set; }
+        public long TelegramId { get; set; }
+
+        // No Email as required
+        public string Notes { get; set; }
     }
 }
