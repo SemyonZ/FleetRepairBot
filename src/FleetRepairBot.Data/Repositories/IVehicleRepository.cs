8df69df9d6dd19edd2d34714fe61866efe92d00c
SemyonZ <Errormini@gmail.com>
2026-06-24 13:30:14 +0300

    Update src/FleetRepairBot.Data/Repositories/IVehicleRepository.cs

--- src/FleetRepairBot.Data/Repositories/IVehicleRepository.cs
+++ src/FleetRepairBot.Data/Repositories/IVehicleRepository.cs
@@ -1,14 +1,16 @@
-using FleetRepairBot.Domain.Entities;
-using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using FleetRepairBot.Domain.Entities;
 
 namespace FleetRepairBot.Data.Repositories
 {
     public interface IVehicleRepository
     {
-        Task<Vehicle> GetByIdAsync(Guid id);
-        Task AddAsync(Vehicle vehicle);
-        Task UpdateAsync(Vehicle vehicle);
-        Task SaveChangesAsync();
+        Task<Vehicle> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<Vehicle> GetByVinAsync(string vin, CancellationToken ct = default);
+        Task<IEnumerable<Vehicle>> GetAllAsync(CancellationToken ct = default);
+        Task AddAsync(Vehicle vehicle, CancellationToken ct = default);
+        Task UpdateAsync(Vehicle vehicle, CancellationToken ct = default);
     }
 }
