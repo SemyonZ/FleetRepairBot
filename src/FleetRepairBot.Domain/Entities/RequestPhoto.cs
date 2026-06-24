9f3b3d9b4bf0dfeceb05e881e19b6ba82414604f
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:16 +0300

    Update src/FleetRepairBot.Domain/Entities/RequestPhoto.cs

--- src/FleetRepairBot.Domain/Entities/RequestPhoto.cs
+++ src/FleetRepairBot.Domain/Entities/RequestPhoto.cs
@@ -4,14 +4,21 @@ namespace FleetRepairBot.Domain.Entities
 {
     public class RequestPhoto
     {
-        public Guid Id { get; set; }
-        public Guid RepairRequestId { get; set; }
-        public RepairRequest RepairRequest { get; set; }
+        public Guid Id { get; private set; }
+        public Guid RepairRequestId { get; private set; }
+        public string Path { get; private set; }
+        public string ThumbnailPath { get; private set; }
+        public DateTime UploadedAt { get; private set; }
 
-        // stored path or blob key
-        public string FileName { get; set; }
-        public string ContentType { get; set; }
-        public long? SizeBytes { get; set; }
-        public DateTime CreatedAt { get; set; }
+        protected RequestPhoto() { }
+
+        public RequestPhoto(Guid repairRequestId, string path, string thumbnailPath = null)
+        {
+            Id = Guid.NewGuid();
+            RepairRequestId = repairRequestId;
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+            ThumbnailPath = thumbnailPath;
+            UploadedAt = DateTime.UtcNow;
+        }
     }
 }
