a50d1adf0f8805da29aee59146dce6a1c8e34e57
SemyonZ <Errormini@gmail.com>
2026-06-24 09:35:50 +0300

    Update src/FleetRepairBot.Domain/Entities/StatusHistory.cs

--- src/FleetRepairBot.Domain/Entities/StatusHistory.cs
+++ src/FleetRepairBot.Domain/Entities/StatusHistory.cs
@@ -4,10 +4,16 @@ namespace FleetRepairBot.Domain.Entities
 {
     public class StatusHistory
     {
-        public int Id { get; set; }
-        public int RepairRequestId { get; set; }
-        public Status OldStatus { get; set; }
-        public Status NewStatus { get; set; }
+        public Guid Id { get; set; }
+        public Guid RepairRequestId { get; set; }
+        public RepairRequest RepairRequest { get; set; }
+
+        public Status From { get; set; }
+        public Status To { get; set; }
         public DateTime ChangedAt { get; set; }
+
+        // optional: who changed (dispatcher TelegramId)
+        public long? ChangedByTelegramId { get; set; }
+        public string Note { get; set; }
     }
 }
