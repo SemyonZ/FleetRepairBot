567e69723c61be5c14c5bffb32ef96e6104f3aa0
SemyonZ <Errormini@gmail.com>
2026-06-24 10:15:20 +0300

    Update src/FleetRepairBot.Data/FleetRepairDbContext.cs

--- src/FleetRepairBot.Data/FleetRepairDbContext.cs
+++ src/FleetRepairBot.Data/FleetRepairDbContext.cs
@@ -1,5 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using FleetRepairBot.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FleetRepairBot.Data
 {
@@ -9,7 +9,6 @@ namespace FleetRepairBot.Data
 
         public DbSet<RepairRequest> RepairRequests { get; set; }
         public DbSet<Driver> Drivers { get; set; }
-        public DbSet<Dispatcher> Dispatchers { get; set; }
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<RequestPhoto> RequestPhotos { get; set; }
         public DbSet<StatusHistory> StatusHistories { get; set; }
@@ -17,51 +16,31 @@ namespace FleetRepairBot.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Driver>(b =>
-            {
-                b.HasKey(x => x.Id);
-                b.Property(x => x.TelegramId).IsRequired();
-            });
-
-            modelBuilder.Entity<Dispatcher>(b =>
-            {
-                b.HasKey(x => x.Id);
-                b.Property(x => x.TelegramId).IsRequired();
-            });
-
-            modelBuilder.Entity<Vehicle>(b =>
-            {
-                b.HasKey(x => x.Id);
-                b.Property(x => x.RegistrationNumber).HasMaxLength(64);
-            });
-
             modelBuilder.Entity<RepairRequest>(b =>
             {
                 b.HasKey(x => x.Id);
-                b.Property(x => x.Title).HasMaxLength(200);
-                b.HasOne(x => x.Vehicle).WithMany().HasForeignKey(x => x.VehicleId).OnDelete(DeleteBehavior.SetNull);
-                b.HasOne(x => x.Driver).WithMany().HasForeignKey(x => x.DriverId).OnDelete(DeleteBehavior.SetNull);
-                b.HasOne(x => x.CreatedByDispatcher).WithMany().HasForeignKey(x => x.CreatedByDispatcherId).OnDelete(DeleteBehavior.SetNull);
+                b.Property(x => x.Description).HasMaxLength(2000);
+                b.OwnsMany<RequestPhoto>(
+                    "Photos",
+                    pb =>
+                    {
+                        pb.WithOwner().HasForeignKey("RepairRequestId");
+                        pb.Property<Guid>("Id");
+                    });
             });
 
             modelBuilder.Entity<RequestPhoto>(b =>
             {
                 b.HasKey(x => x.Id);
-                b.HasOne(x => x.RepairRequest).WithMany(r => r.Photos).HasForeignKey(x => x.RepairRequestId).OnDelete(DeleteBehavior.Cascade);
             });
 
-            modelBuilder.Entity<StatusHistory>(b =>
-            {
-                b.HasKey(x => x.Id);
-                b.HasOne(x => x.RepairRequest).WithMany(r => r.StatusHistories).HasForeignKey(x => x.RepairRequestId).OnDelete(DeleteBehavior.Cascade);
-            });
+            modelBuilder.Entity<StatusHistory>(b => b.HasKey(x => x.Id));
+            modelBuilder.Entity<Driver>(b => b.HasKey(x => x.Id));
+            modelBuilder.Entity<Vehicle>(b => b.HasKey(x => x.Id));
+            modelBuilder.Entity<AuditLog>(b => b.HasKey(x => x.Id));
 
-            modelBuilder.Entity<AuditLog>(b =>
-            {
-                b.HasKey(x => x.Id);
-            });
+            // store enum as int by default
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
